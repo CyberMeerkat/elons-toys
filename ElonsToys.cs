086312660a70572a155c268c16781c3b5bcd20d2
CyberMeerkat <apouyaud@gmail.com>
2022-09-27 15:37:33 +0200

    first draft

--- ElonsToys.cs
+++ ElonsToys.cs
@@ -2,23 +2,24 @@ using System;
 
 class RemoteControlCar
 {
-    public static RemoteControlCar Buy()
-    {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
-    }
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+    public string DistanceDisplay() =>
+        $"Driven {distanceCovered} meters";
 
-    public string DistanceDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
-    }
-
-    public string BatteryDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
-    }
+    public string BatteryDisplay() =>
+        batteryLife > 0 ? $"Battery at {batteryLife}%" : "Battery empty";
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (batteryLife > 0)
+        {
+            distanceCovered += 20;
+            batteryLife -= 1;
+            if (batteryLife <= 0)
+                BatteryDisplay();
+        }
     }
+
+    private int distanceCovered = 0;
+    private int batteryLife = 100;
 }
